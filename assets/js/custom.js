690a990e69e6f123a1393b063e46dbcc867a2009
YanaIgnatkova <43076614+YanaIgnatkova@users.noreply.github.com>
2020-11-10 23:05:51 +0300

    Add files via upload

---
+++ assets/js/custom.js
@@ -0,0 +1,7 @@
+$(function(){
+        $("a[href^='#']").click(function(){
+                var _href = $(this).attr("href");
+                $("html, body").animate({scrollTop: $(_href).offset().top+"px"}, 5000);
+                return false;
+        });
+});
