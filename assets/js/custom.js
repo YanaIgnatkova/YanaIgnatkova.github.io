9bf414a180c6bfc68fdc6f020c6ea9473eaf536f
YanaIgnatkova <43076614+YanaIgnatkova@users.noreply.github.com>
2020-11-26 08:37:08 +0300

    Add files via upload

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -5,3 +5,22 @@ $(function(){
                 return false;
         });
 });
+
+
+
+function openPrice(evt, priceName) {
+    var i, tabcontent, tablinks;
+
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+
+    tablinks = document.getElementsByClassName("tablinks");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].className = tablinks[i].className.replace("active", "");
+    }
+
+    document.getElementById(priceName).style.display = "block";
+    evt.currentTarget.className += " active";
+}
